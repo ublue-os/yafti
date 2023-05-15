56fbb320c1ef64b2b7a8f4d17e72c6f00bb67e0c
Arcitec <38923130+Arcitec@users.noreply.github.com>
2023-05-15 23:38:05 +0200

    feat: temporary backwards compatibility with old config location

--- yafti/app.py
+++ yafti/app.py
@@ -19,6 +19,7 @@ import hashlib
 import gbulb
 import yaml
 from gi.repository import Adw
+from pathlib import Path
 
 from yafti.parser import Config, YaftiRunModes
 from yafti.screen.window import Window
@@ -32,7 +33,16 @@ class Yafti(Adw.Application):
 
     def run(self, *args, **kwargs):
         configured_mode = self.config.properties.mode
-        _p = self.config.properties.path.expanduser()
+        _p: Path = self.config.properties.path.expanduser()
+        # TODO(GH-#103): Remove this prior to 1.0 release. Start.
+        _old_p = Path("~/.config/yafti-last-run").expanduser()
+        if _old_p.exists() and _old_p.resolve() != _p.resolve():
+            if not _p.parent.is_dir():
+                _p.parent.mkdir(mode=0o755, parents=True, exist_ok=True)
+            if _p.is_file():
+                _p.unlink()
+            _old_p.rename(_p)
+        # TODO(GH-#103): End.
         if configured_mode == YaftiRunModes.disable:
             return
 
